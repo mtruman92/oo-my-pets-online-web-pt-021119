40e461f177537622a8a82f6ac7ad2bf28e151d24
Mickae Truman <mtruman@nyit.edu>
2019-03-20 22:53:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
   
   attr_reader :name
+  attr_accessor :mood
   
-  def initialize(name)
+  def initialize(name, mood)
     @name = name
+    @mood = mood
   end
   # code goes here
 end
